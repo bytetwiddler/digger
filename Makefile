9559f176d3fdbaa51604a5ba3f719a997bfc1cbe
bill.bohlman@lanecountyor.gov <bill.bohlman@lanecountyor.gov>
2025-03-05 16:20:38 -0800

    added gelf logging

--- Makefile
+++ Makefile
@@ -1,8 +1,18 @@
+.PHONY: build clean
 APP := digger
 APP_VERSION := 0.1.0
 APP_BUILD := $(shell git rev-parse --short HEAD)
 APP_DATE := $(shell date -u '+%Y-%m-%d_%H:%M:%S')
 LDFLAGS := -X main.version=$(APP_VERSION) -X main.build=$(APP_BUILD) -X main.date=$(APP_DATE)
 
-build:
+build: clean deps
 	go build -ldflags "$(LDFLAGS)" -o $(APP) main.go
+
+run: build
+	./$(APP)
+
+clean:
+	rm -f $(APP)
+
+deps:
+	go mod tidy
--- main.go
+++ main.go
@@ -3,10 +3,23 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"log"
 	"net"
 	"os"
 	"strconv"
+
+	"github.com/Graylog2/go-gelf/gelf"
+	"gopkg.in/yaml.v2"
 )
+
+type Config struct {
+	Log struct {
+		Level string `yaml:"level"`
+		Gelf  struct {
+			Address string `yaml:"address"`
+		} `yaml:"gelf"`
+	} `yaml:"log"`
+}
 
 type Site struct {
 	Hostname   string `csv:"hostname"`
@@ -16,6 +29,33 @@ type Site struct {
 }
 
 type Sites []Site
+
+func (s *Sites) writeToFile(filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	header := []string{"Hostname", "Port", "EntityName", "IP"}
+	err = writer.Write(header)
+	if err != nil {
+		return err
+	}
+
+	for _, site := range *s {
+		record := []string{site.Hostname, strconv.Itoa(site.Port), site.EntityName, site.IP}
+		err = writer.Write(record)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
 
 func (s *Sites) readFromFile(filename string) error {
 	file, err := os.Open(filename)
@@ -39,6 +79,7 @@ func (s *Sites) readFromFile(filename string) error {
 			Hostname:   record[0],
 			Port:       port,
 			EntityName: record[2],
+			IP:         record[3],
 		}
 		*s = append(*s, site)
 	}
@@ -50,26 +91,55 @@ func dig(sites *Sites) error {
 	for i := range *sites {
 		ip, err := net.LookupIP((*sites)[i].Hostname)
 		if err != nil {
-			fmt.Println("Error:", err)
+			log.Printf("Error: %v", err)
 			return err
 		}
-		(*sites)[i].IP = ip[0].String()
+		if (*sites)[i].IP != ip[0].String() {
+			fmt.Printf("%v IP changed from %v to %v\n", (*sites)[i].Hostname, (*sites)[i].IP, ip[0].String())
+			log.Printf("%v IP changed from %v to %v", (*sites)[i].Hostname, (*sites)[i].IP, ip[0].String())
+			(*sites)[i].IP = ip[0].String()
+		}
+		fmt.Printf("%v IP has not change %v <-> %v\n", (*sites)[i].Hostname, (*sites)[i].IP, ip[0].String())
+		log.Printf("%v IP has not change %v <-> %v", (*sites)[i].Hostname, (*sites)[i].IP, ip[0].String())
 	}
 	return nil
 }
 
 func main() {
-	var sites Sites
-	err := sites.readFromFile("sites.csv")
+	// Read configuration
+	configFile, err := os.Open("config.yaml")
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		log.Fatalf("Error opening config file: %v", err)
 	}
-	fmt.Println(sites)
+	defer configFile.Close()
+
+	var config Config
+	decoder := yaml.NewDecoder(configFile)
+	err = decoder.Decode(&config)
+	if err != nil {
+		log.Fatalf("Error decoding config file: %v", err)
+	}
+
+	// Set up GELF logger
+	gelfWriter, err := gelf.NewWriter(config.Log.Gelf.Address)
+	if err != nil {
+		log.Fatalf("Error setting up GELF logger: %v", err)
+	}
+	log.SetOutput(gelfWriter)
+
+	var sites Sites
+	err = sites.readFromFile("sites.csv")
+	if err != nil {
+		log.Fatalf("Error reading sites file: %v", err)
+	}
+
 	err = dig(&sites)
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		log.Fatalf("Error in dig function: %v", err)
 	}
-	fmt.Println(sites)
+
+	err = sites.writeToFile("sites.csv")
+	if err != nil {
+		log.Fatalf("Error writing sites file: %v", err)
+	}
 }
